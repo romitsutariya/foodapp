98f89dd8f3ce7983781929e8ebdb0bd6bab7c5c4
romitsutariya <=romitsutariya@gmail.com>
2018-11-01 23:06:01 +0530

    third commit

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-app.use(express.static("/"));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+app.use(express.static("public/"));
+
+ app.get('/', function (req, res) {
+    res.sendFile(__dirname+"/index.html");
+ })
  app.post('/', function (req, res) {
-    res.sendFile(__dirname);
+     console.log(req.body);
+    res.sendStatus(200);
  })
 
 var server = app.listen(3000, function () {
    var host = server.address().address
    var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Example app listening at http://%s:%s", host, port)
 })
