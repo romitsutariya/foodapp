97a43a0a86a93ce14bde4f17757526233f269839
romitsutariya <=romitsutariya@gmail.com>
2018-11-30 21:50:37 +0530

    modification 30/11/2018

--- index.js
+++ index.js
@@ -1,47 +1,36 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 var MongoClient = require('mongodb').MongoClient;
 var app = express();
+
+//this  uel for  connectin mango
 const URL="mongodb://romit:romit123@ds227469.mlab.com:27469/k501"
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
 app.use(bodyParser.json())
+app.use(cors())
 app.use(express.static("public/"));
 
+ //this is for  loading first page   
  app.get('/', function (req, res) {
     res.sendFile(__dirname+"/index.html");
  })
+
+
+ //this is  for loading balance
  app.get('/balance', function (req, res) {
-    MongoClient.connect(URL, { useNewUrlParser: true },function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("k501");
-        dbo.collection("balance").findOne({}, function(err, result) {
-            if (err) throw err;
-            res.send(result);
-            db.close();
-          });
-      });
- })
- app.post('/', function (req, res) {
-     console.log(req.body);
-     req.body.balance = (parseInt(req.body.balance)-parseInt(req.body.amount));
-    MongoClient.connect(URL, { useNewUrlParser: true },function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("k501");
-         dbo.collection("balance").updateOne({ u:'101' }, { $set: {amount:req.body.balance} }, function(err, res) {
-         if (err) throw err;
-         console.log("1 document updated");
-         });
-         dbo.collection("food").insertOne(req.body, function(err, res) {
-          if (err) throw err;
-          console.log("1 document inserted");
-          db.close();
-        });
-      });
-      res.send(req.body.balance+"");
+    
  })
 
+ 
+ //this is for handling  post request
+ app.post('/', function (req, res) {
+     console.log(req.body);
+ })
+
+
+//server configuration file
 var server = app.listen(3000, function () {
    var host = server.address().address
    var port = server.address().port
